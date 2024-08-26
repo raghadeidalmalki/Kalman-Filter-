30bac500b0a8009310bcfe9572c7968948b14a4b
Raghad AlMalki <raghadeidalmalki@gmail.com>
2024-08-26 04:53:39 +0300

    Update KalmenFilter.cpp

--- src/KalmenFilter.cpp
+++ src/KalmenFilter.cpp
@@ -1,96 +1,29 @@
-/** 
- * Write a function 'filter()' that implements a multi-
- *   dimensional Kalman Filter for the example given
- */
+#include "kalman_filter.h"
 
-#include <iostream>
-#include <vector>
-#include "Dense"
-
-using std::cout;
-using std::endl;
-using std::vector;
-using Eigen::VectorXd;
-using Eigen::MatrixXd;
-
-// Kalman Filter variables
-VectorXd x;	// object state
-MatrixXd P;	// object covariance matrix
-VectorXd u;	// external motion
-MatrixXd F; // state transition matrix
-MatrixXd H;	// measurement matrix
-MatrixXd R;	// measurement covariance matrix
-MatrixXd I; // Identity matrix
-MatrixXd Q;	// process covariance matrix
-
-vector<VectorXd> measurements;
-void filter(VectorXd &x, MatrixXd &P);
-
-
-int main() {
-  /**
-   * Code used as example to work with Eigen matrices
-   */
-  // design the KF with 1D motion
-  x = VectorXd(2);
-  x << 0, 0;
-
-  P = MatrixXd(2, 2);
-  P << 1000, 0, 0, 1000;
-
-  u = VectorXd(2);
-  u << 0, 0;
-
-  F = MatrixXd(2, 2);
-  F << 1, 1, 0, 1;
-
-  H = MatrixXd(1, 2);
-  H << 1, 0;
-
-  R = MatrixXd(1, 1);
-  R << 1;
-
-  I = MatrixXd::Identity(2, 2);
-
-  Q = MatrixXd(2, 2);
-  Q << 0, 0, 0, 0;
-
-  // create a list of measurements
-  VectorXd single_meas(1);
-  single_meas << 1;
-  measurements.push_back(single_meas);
-  single_meas << 2;
-  measurements.push_back(single_meas);
-  single_meas << 3;
-  measurements.push_back(single_meas);
-
-  // call Kalman filter algorithm
-  filter(x, P);
-
-  return 0;
+KalmanFilter::KalmanFilter() {
 }
 
+KalmanFilter::~KalmanFilter() {
+}
 
-void filter(VectorXd &x, MatrixXd &P) {
-    for (unsigned int n = 0; n < measurements.size(); ++n) {
-        // Measurement update step
-        VectorXd z = measurements[n];
-        VectorXd y = z - H * x; // Measurement residual
-        MatrixXd S = H * P * H.transpose() + R; // Residual covariance
-        MatrixXd K = P * H.transpose() * S.inverse(); // Kalman gain
-        
-        // Update state estimate
-        x = x + K * y;
-        
-        // Update covariance estimate
-        P = (I - K * H) * P;
-        
-        // Prediction step
-        x = F * x + u;
-        P = F * P * F.transpose() + Q;
+void KalmanFilter::Predict() {
+  x_ = F_ * x_;
+  MatrixXd Ft = F_.transpose();
+  P_ = F_ * P_ * Ft + Q_;
+}
 
-        // Output the state and covariance
-        cout << "x=" << endl <<  x << endl;
-        cout << "P=" << endl <<  P << endl;
-    }
+void KalmanFilter::Update(const VectorXd &z) {
+  VectorXd z_pred = H_ * x_;
+  VectorXd y = z - z_pred;
+  MatrixXd Ht = H_.transpose();
+  MatrixXd S = H_ * P_ * Ht + R_;
+  MatrixXd Si = S.inverse();
+  MatrixXd PHt = P_ * Ht;
+  MatrixXd K = PHt * Si;
+
+  //new estimate
+  x_ = x_ + (K * y);
+  long x_size = x_.size();
+  MatrixXd I = MatrixXd::Identity(x_size, x_size);
+  P_ = (I - K * H_) * P_;
 }
